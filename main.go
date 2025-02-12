bfed71363615bcf0a97b95681e64c44ab69e2b31
Nicholas Cecere <nick@bitop.dev>
2025-02-11 22:11:42 -0500

    Add GitHub Actions release workflow and GoReleaser config
    - Add GitHub Actions workflow to build and release on tag push
    - Add GoReleaser configuration for multi-platform builds
    - Add version information to CLI

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ncecere/navigatorctl/cmd"
 )
 
+var (
+	version = "dev"
+)
+
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Printf("navigatorctl version %s\n", version)
+		os.Exit(0)
+	}
 	cmd.Execute()
 }
